36be61a380d6dc46b744e8a6c96832ab366bcf27
unknown <skenine.info@protonmail.com>
2022-04-07 18:06:37 +0300

    Updates

---
+++ precache-manifest.0d5259181683980ad9d69d76b8cf136a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a83b375b01d0b7d9a3ce",
+    "url": "/web/static/css/main.babbab69.chunk.css"
+  },
+  {
+    "revision": "a83b375b01d0b7d9a3ce",
+    "url": "/web/static/js/main.a83b375b.chunk.js"
+  },
+  {
+    "revision": "dce9d101c10db6ff5002",
+    "url": "/web/static/js/1.dce9d101.chunk.js"
+  },
+  {
+    "revision": "05773d53a80748feb222",
+    "url": "/web/static/js/runtime~main.05773d53.js"
+  },
+  {
+    "revision": "c7d354ff10c868752f1b032e5b0e55e8",
+    "url": "/web/static/media/sample.c7d354ff.pdf"
+  },
+  {
+    "revision": "774f823acdaef2416dd456f6a2b95fc2",
+    "url": "/web/index.html"
+  }
+];
