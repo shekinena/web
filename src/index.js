3d2902844c1dc5a415f53f6cc6f08adc3bbeb04d
unknown <shekine.info@protonmail.com>
2022-04-07 23:06:01 +0300

    Second Commit

--- src/index.js
+++ src/index.js
@@ -16,12 +16,18 @@ ReactDOM.render(
     <Router>
     <ResponsiveAppBar />
     <Routes>
-      <Route path="" element={< Home />} />
+      <Route path="/web" element={< Home />} />
+      <Route path="/" element={< Home />} />
       <Route path="home" element={< Home />} />
+      <Route path="web/home" element={< Home />} />
       <Route path="about" element={<About />} />
+      <Route path="web/about" element={<About />} />
       <Route path="thesis" element={<Thesis />} />
+      <Route path="we/thesis" element={<Thesis />} />
       <Route path="publications" element={<Publication />} />
+      <Route path="web/publications" element={<Publication />} />
       <Route path="contact" element={<Contact />} />
+      <Route path="web/contact" element={<Contact />} />
       <Route path="*" element={<Missing />} status={404} />
     </Routes>
     <Footer />
