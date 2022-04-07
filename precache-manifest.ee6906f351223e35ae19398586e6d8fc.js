3803b1c21b6f3ee4edf447928fa54291fff3d691
unknown <skenine.info@protonmail.com>
2022-04-07 18:13:42 +0300

    Updates

---
+++ precache-manifest.ee6906f351223e35ae19398586e6d8fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "095f851e44c02a5cb587",
+    "url": "/web/static/css/main.babbab69.chunk.css"
+  },
+  {
+    "revision": "095f851e44c02a5cb587",
+    "url": "/web/static/js/main.095f851e.chunk.js"
+  },
+  {
+    "revision": "dce9d101c10db6ff5002",
+    "url": "/web/static/js/1.dce9d101.chunk.js"
+  },
+  {
+    "revision": "05773d53a80748feb222",
+    "url": "/web/static/js/runtime~main.05773d53.js"
+  },
+  {
+    "revision": "c7d354ff10c868752f1b032e5b0e55e8",
+    "url": "/web/static/media/sample.c7d354ff.pdf"
+  },
+  {
+    "revision": "62486f48f21e9387d9a545d919877882",
+    "url": "/web/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c6ede741deb0b372771c02999e2eeeb9.js"
+  "/web/precache-manifest.ee6906f351223e35ae19398586e6d8fc.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/web/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
